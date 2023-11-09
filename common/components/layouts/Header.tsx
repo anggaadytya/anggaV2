d8a00d9ea5a9faa91928c38608faabc925d6a61d
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2023-11-09 17:27:23 +0700

    feat/header : update header 1

--- common/components/layouts/Header.tsx
+++ common/components/layouts/Header.tsx
@@ -1,13 +1,13 @@
-"use client"
-import React from 'react'
 import Link from 'next/link'
+
+import React from 'react'
 
 export default function Header() {
   return (
     <header className="bg-gray-300 shadow-xl md:w-[70%] mx-auto my-4 h-6 p-8 sticky top-0 flex items-center justify-center z-50 rounded-2xl">
       <nav className="w-full flex justify-between items-center">
         <h1>Muhammad Angga Adytya</h1>
-        <ul id='nav' className="hidden md:flex gap-4 ">
+        <ul id="nav" className="hidden md:flex gap-4 ">
           <li>
             <Link href="/">Home</Link>
           </li>
@@ -22,15 +22,6 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-        <button 
-        className='md:hidden'
-        onClick={() => {
-          const nav = document.getElementById('nav')
-          if (nav) {
-            nav.classList.toggle('hidden')
-          }
-        }}
-        >X</button>
     </header>
   )
 }
